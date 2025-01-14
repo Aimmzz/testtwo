a1fb70d74416cf22f93fd11e5e38f4dfb6b68768
Aimmzz <rohimkurniawan456@gmail.com>
2025-01-14 10:14:11 +0700

    feat(): add api key for review test

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -6,7 +6,7 @@ package org.example
 import org.example.repository.WeatherRepository
 
 fun main() {
-    val apiKey = ""
+    val apiKey = "eccbeb18979a1495da6750f22f0aaea3"
     val city = "Jakarta"
     val repository = WeatherRepository()
     repository.getWeatherForecast(city, apiKey)
